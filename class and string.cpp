4ede3280109335babb7a6a42fc78bb471472d1bd
VISHALkumar0087 <vk3926173@gmail.com>
2025-10-15 11:48:53 +0530

    Add files via upload

---
+++ class and string.cpp
@@ -0,0 +1,36 @@
+// You are using GCC
+
+#include <iostream>
+using namespace std;
+//   class car {
+//       public :
+//       int model_no ;
+//       str type;
+//   }
+//   int main(){
+//       car vishal_ki_car;
+//       vishal_ki_car.type="BMW";
+//       vishal_ki_car.model_no=12;
+      
+//       car nikhil_ki_car;
+//       nikhil_ki_car.type="ALTO";
+//       nikhil_ki_car.model_no=800;
+//   }
+ class student {
+     public :
+     int roll_no;
+     string name;
+     string schlr;
+
+ };
+ 
+ int main (){
+     student v;
+     v.roll_no=9;
+     v.name="vishal";
+     v.schlr="yes";
+     cout<<"roll_no: "<<v.roll_no<<endl;
+     cout<<"name: "<<v.name<<endl;
+     cout<<"schlr: "<<v.schlr<<endl;
+    
+ }
