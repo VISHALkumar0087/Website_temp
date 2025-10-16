7226dbbf74bbd1f32a6cb21d467b2d1ff348a561
VISHALkumar0087 <vk3926173@gmail.com>
2025-10-16 10:47:00 +0530

    Add files via upload

---
+++ bubble short array.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+int n = 5;
+int arr[n]={5,4,3,2,1};
+cout<<"Initial Array is : ";
+for(int i = 0; i < n; i++){
+    cout<<arr[i]<<" ";
+}
+
+for(int i = 0; i < n-1; i++){
+    for(int j = 0; j < n-i-1; j++){
+        if(arr[j]>arr[j+1]){
+            swap(arr[j],arr[j+1]);
+        }
+    }
+}
+cout<<endl;
+cout<<"Sorted Array is: ";
+for (int i = 0; i < n; i++)cout<<arr[i]<<" ";
+    return 0;
+}
